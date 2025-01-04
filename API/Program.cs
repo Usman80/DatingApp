739ac5114abac6c794552a1d7454f00c7032572a
Usman Rashid <usman.bytepeach@gmail.com>
2025-01-04 15:49:44 +0500

    Exceptions Committed

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add service to the container
@@ -7,6 +8,7 @@ builder.Services.AddIdentityService(builder.Configuration);
 
 var app = builder.Build();
 // Configue the Http request pipeline
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().
     WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.UseAuthentication();
